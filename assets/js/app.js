8276e7cdeec512ced96c662e1da9212fff846fbf
mohitgoyat525 <montyits71@gmail.com>
2024-10-02 10:33:20 +0530

    add-changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,24 +30,52 @@ $(".yoga-slider-first").slick({
   ],
 });
 // Third slick-slider
-$(document).ready(function () {
-  $(".big-img-slider").slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: ".second-row-slider",
-    infinite: true,
-  });
+ $(document).ready(function () {
+   $(".big-img-slider").slick({
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     arrows: false,
+     fade: true,
+     asNavFor: ".small-img-slider",
+     infinite: true,
+   });
 
-  $(".second-row-slider").slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: ".big-img-slider",
-    arrows: false,
-    centerMode: true,
-    focusOnSelect: true,
-    infinite: true,
-    variableWidth: true,
-  });
-});
+   $(".small-img-slider").slick({
+     slidesToShow: 4,
+     slidesToScroll: 1,
+     asNavFor: ".big-img-slider",
+     focusOnSelect: true,
+     infinite: true,
+     variableWidth: true,
+     responsive: [
+       {
+         breakpoint: 1024,
+         settings: {
+           slidesToShow: 3,
+           slidesToScroll: 1,
+           infinite: true,
+           centerMode: true,
+           dots: false,
+         },
+       },
+       {
+         breakpoint: 600,
+         settings: {
+           slidesToShow: 2,
+           centerMode: true,
+           slidesToScroll: 1,
+         },
+       },
+       {
+         breakpoint: 480,
+         settings: {
+           slidesToShow: 1,
+           centerMode: true,
+           slidesToScroll: 1,
+         },
+       },
+     ],
+   });
+ });
+  
+
